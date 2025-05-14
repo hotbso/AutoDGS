90d1279b5b6dac3c2d892945387cc6caca7087cd
Holger Teutsch <holger.a.teutsch@gmail.com>
2025-05-14 11:39:04 +0200

    improve build process

--- DGSs-Safedock-T2-24/test_install.sh
+++ DGSs-Safedock-T2-24/test_install.sh
@@ -1,9 +1,13 @@
 RESDIR=/e/X-Plane-12-test/Resources/plugins/AutoDGS/resources
 if [ -d $RESDIR ]
 then
-    for f in *
+    for f in Safedock-T2-24* 
     do
         [[ $f == *-base.obj ]] && continue
-        [[ $f == *.obj || $f == *.png || $f == *.dds ]] && cp -p $f $RESDIR/.
+        if [[ $f == *.obj || $f == *.png || $f == *.dds ]]
+        then
+            echo "Copy \"$f\""
+            cp -p $f $RESDIR/.
+        fi
     done
 fi
